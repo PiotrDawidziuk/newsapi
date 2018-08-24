88a5e679a32a6337ddcc619d73fb90103e051087
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-08-24 23:36:21 +0200

    NewsController added

---
+++ src/main/java/newsapi/controller/NewsController.java
@@ -0,0 +1,20 @@
+package newsapi.controller;
+
+import newsapi.objects.Country;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class NewsController {
+
+    // new Country? NewsCategory?
+
+    @RequestMapping("/news")
+    public String greeting(@RequestParam(value="Country", defaultValue="pl") String name) {
+        return "news";
+
+    }
+}
