8152d5660c23448690bfe724c6655202c867d31b
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-08-28 12:59:47 +0200

    NewsApiService edited

--- src/main/java/newsapi/service/NewsApiService.java
+++ src/main/java/newsapi/service/NewsApiService.java
@@ -5,24 +5,22 @@ import newsapi.objects.Country;
 import newsapi.objects.NewsCategory;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class NewsApiService {
 
     private final RestTemplate restTemplate;
-    private final String apiKey;
+    private final String apiKey = "62f96aafcdca445fbfd72d61d8d09914";
 
-    public NewsApiService(RestTemplate restTemplate, String apiKey) {
+    public NewsApiService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.apiKey = apiKey;
     }
-    
+
 
     public List<Article> findArticlesByCountryAndCategory(Country country, NewsCategory category) {
         final String url = "https://newsapi.org/v1/articles";
-        List<Article> articles = restTemplate.getForObject(url+"?category="+category+"&country="+country,
-                List.class);
+        List<Article> articles = restTemplate.getForObject(url+"?category="+category+"&country="+country+
+                        &apiKey, List.class);
         return articles;
     }
 
