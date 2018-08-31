68664b8962ee68bdba252581e0a09c7e528eaf75
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-08-31 17:08:24 +0200

    controllers updated

--- src/main/java/newsapi/controller/NewsController.java
+++ src/main/java/newsapi/controller/NewsController.java
@@ -4,7 +4,6 @@ import newsapi.objects.MyNewsResponse;
 import newsapi.objects.NewsApiResponse;
 import newsapi.service.NewsApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +16,8 @@ public class NewsController {
 
    @GetMapping("/news/{country}/{category}")
     public MyNewsResponse news(@PathVariable ("country") String country, @PathVariable ("category") String category) {
-
        NewsApiResponse newsApiResponse = newsApiService.findArticlesByCountryAndCategory(country,category);
-
         MyNewsResponse myNewsResponse = new MyNewsResponse(country,category, newsApiResponse.getArticles());
-
        return myNewsResponse;
     }
-
 }
